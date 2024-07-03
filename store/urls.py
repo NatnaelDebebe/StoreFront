0fb5aee737706442efe7c77f3e1a32043e291800
Natnael <natnaeldebeb463@gmail.com>
2024-07-03 23:53:37 +0300

    changes on sotre/view add new class called viewset, changes in the urls, changes on delete method add new methode called destroy()

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 from . import views
 
+router=SimpleRouter()
+router.register('products',views.ProductViewSet)
+router.register('collections',views.CollectionViewSet)
+router.urls
 
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collection/',views.CollectionList.as_view()),
-    path('collection/<int:pk>/', views.CollectionDetial.as_view(), name='collection-detail'),
-
-]
+urlpatterns = router.urls
--- store/views.py
+++ store/views.py
@@ -6,49 +6,45 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.mixins import ListModelMixin,CreateModelMixin
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
-from .models import Product,Collection,Order
+from rest_framework.viewsets import ModelViewSet
+from .models import Product,Collection,OrderItem
 from django.db.models import Count
 from .serializers import ProductSerializer,CollectionSerializer
-# Create your views here.
 
-class ProductList(ListCreateAPIView):
-    queryset=Product.objects.select_related('collection').all()
-    serializer_class=ProductSerializer
-    
-    def get_serializer_context(self):
-        return {'request': self.request}
-    
-   
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
+class ProductViewSet(ModelViewSet):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
 
-    def delete(self,request,pk):
-        product=get_object_or_404(Product,pk=pk)
-        if product.orderitems.count()>0:
-            return Response({'error': 'product cannot be deleted '},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_serializer_context(self):
+        return {'request': self.request}
     
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count()>0:
+            return Response({'error': 'product cannot be deleted '},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().destroy(request, *args, **kwargs)
 
+    
+    
        
-
-
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset=Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class=CollectionSerializer
-    
-class CollectionDetial(RetrieveUpdateDestroyAPIView):
-    queryset=Collection.objects.annotate(products_count=Count('products'))
-    serializer_class=CollectionSerializer
 
-    def delete(self,request,pk):
-        collection=get_object_or_404(Collection, pk=pk)
-        if collection.products.count() >0:
+    def destroy(self, request, *args, **kwargs):
+        if Collection.objects.filter(id=kwargs['pk']).count() >0:
             return Response({'error':"product cannot be deleted"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+
+    # def delete(self,request,pk):
+    #     collection=get_object_or_404(Collection, pk=pk)
+        
+    #     collection.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
+    
+
+    
 
     
 
